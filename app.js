4d357b7576bd18474cb77fb065d1fb54dcc9fc07
Prabhjot Dev <prabhjotdev5@gmail.com>
2024-03-16 12:18:02 -0400

    Updated Subscription Backed to handle CRUD operations

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const express = require("express");
 const db = require("./db.js");
 
 const app = express();
+var cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.use(cors());
+app.options('*',cors());
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();  
+}
+app.use(allowCrossDomain);
+
+app.get("/subscription", (req, res) => {
   db.query("SELECT * FROM Subscriptions", (err, results) => {
     if (err) {
       console.error("Error executing query:", err);
@@ -36,7 +47,7 @@ app.get("/subscriptionById", (req, res) => {
   });
 });
 
-app.post("/subscription", (req, res) => {
+app.post("/AddSubscription", (req, res) => {
   let { Name, Cost, BillingPeriod, NextBillingDate, Status } = req.body;
 
   // Basic validation
@@ -75,8 +86,8 @@ app.post("/subscription", (req, res) => {
   );
 });
 
-app.delete("/subscription", (req, res) => {
-  let id = req.body.Id;
+app.delete("/subscription/:id", (req, res) => {
+  let id = req.params.id;
   if (id === undefined || isNaN(id)) {
     res.status(400).send("Invalid or missing ID parameter");
     return;
@@ -90,6 +101,42 @@ app.delete("/subscription", (req, res) => {
     }
     res.status(200).send(`Deleted row with Id=${id}`);
   });
+});
+
+app.put("/subscription/:id", (req, res)=>{
+    const id = req.params.id;
+    const {Name, Cost, BillingPeriod, NextBillingDate, Status} = req.body;
+    // Basic validation
+  if (
+    !Name ||
+    isNaN(Cost) ||
+    Cost <= 0 ||
+    !BillingPeriod ||
+    !NextBillingDate ||
+    !Status
+  ) {
+    res.status(400).send("Invalid or missing parameters");
+    return;
+  }
+
+  // Validate NextBillingDate is a valid date
+  const date = new Date(NextBillingDate);
+
+  if (isNaN(date.getTime())) {
+    res.status(400).send("Invalid NextBillingDate");
+    return;
+  }
+
+  const formattedDate = date.toISOString().split("T")[0];
+
+    db.query(`UPDATE Subscriptions SET Name='${Name}', Cost=${Cost}, BillingPeriod='${BillingPeriod}', NextBillingDate='${formattedDate}', Status='${Status}' WHERE ID=${id}`, (err, results)=>{
+        if (err) {
+            console.error("Error executing query:", err);
+            res.status(500).send("Internal Server Error");
+            return;
+          }
+        });
+    res.status(200).send(`Updated row with Id=${id}`);
 });
 
 // Start the server
